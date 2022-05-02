20e3233fd1dffce757d39727b9bdb744dda47e51
Maria Clara <mcfranca0107@gmail.com>
2022-05-02 15:12:32 -0300

    classe programador

--- lib/model.dart
+++ lib/model.dart
@@ -5,22 +5,37 @@ class Pessoa {
   String? cpf;
   DateTime? nascimento;
   int? idade;
-  double? salario;
-  bool? programador;
+
+  int? getIdade(){
+    if(nascimento == null){
+      return null;
+    }
+    final hoje = DateTime.now();
+    final idade = hoje.difference(nascimento!);
+    return idade.inDays ~/ 365;
+  }
 
   @override
   String toString(){
-    return "{nome='$nome', cpf='$cpf', nascimento=$nascimento, idade=$idade, sexo=$Sexo.MASCULINO, salario=$salario, programador=$programador}";
+    return "{nome='$nome', cpf='$cpf', nascimento=$nascimento, idade=${getIdade()}, sexo=$Sexo.MASCULINO}";
+  }
+}
+
+class Programador extends Pessoa{
+  double? salario;
+
+  @override
+  String toString(){
+    return "{nome='$nome', cpf='$cpf', nascimento=$nascimento, idade=${getIdade()}, sexo=$Sexo.MASCULINO, salario=$salario}";
   }
 }
 
 void mainEntidades(){
-  Pessoa pessoa = Pessoa();
-  pessoa.nome = "Joao";
-  pessoa.cpf = "095.523.234-33";
-  pessoa.nascimento = DateTime(2003,09,25);
-  pessoa.idade = 18;
-  pessoa.salario = 5600.0;
-  pessoa.programador = true;
-  print(pessoa.toString());
+  Programador prog = Programador();
+  prog.nome = "Joao";
+  prog.cpf = "095.523.274-33";
+  prog.nascimento = DateTime(2003,09,25);
+  prog.idade = 18;
+  prog.salario = 5600.0;
+  print(prog.toString());
 }
